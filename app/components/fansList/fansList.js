5665ee5521131e83f1bdd030629db2c4a9b80eb8
uxin603s <uxin603s@gmail.com>
2016-12-07 16:51:24 +0800

    update

---
+++ app/components/fansList/fansList.js
@@ -0,0 +1,28 @@
+angular.module('app').component("fansList",{
+bindings:{
+},
+templateUrl:'app/components/fansList/fansList.html?t='+Date.now(),
+controller:["$scope","cache",
+function($scope,cache){
+	$scope.status_arr={
+		0:"下架",
+		1:"上架",
+		2:"申訴",
+	}
+	var post_data={
+		func_name:"FansList::getList",
+		arg:{
+			where_list:$scope.where_list,
+			limit:{page:0,count:10},
+		},
+	}
+	$.post("ajax.php",post_data,function(res){
+		if(res.status){
+			$scope.list=res.list
+		}else{
+			$scope.list=[];
+		}
+		$scope.$apply();
+	},"json")
+}],
+})
--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/angular.min.js"></script>
+<script src="js/localForage-1.4.2.min.js"></script>
+<script src="app/app.js"></script>
+<script src="app/factories/cache/cache.js"></script>
+<script src="app/components/fansList/fansList.js?t=<?=time();?>"></script>
+<script src="app/factories/cache/cache.js?t=<?=time();?>"></script>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css?t=<?=time();?>">
+<link rel="stylesheet" type="text/css" href="css/index.css?t=<?=time();?>">
+</head>
+<body ng-app="app">
+	<fans-list ng-if="!cache.not_finish_flag" ></fans-list>
+</body>
+</html>
